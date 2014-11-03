824dfac1363f6158a6d66ccb7bf005433707447d
Pieter Slabbert <zan.xhipe@gmail.com>
2014-11-03 20:31:03 +0200

    Test write out all images

--- packer.go
+++ packer.go
@@ -10,14 +10,20 @@ import (
 	"sort"
 
 	"image"
-	_ "image/png"
+	"image/draw"
+	"image/png"
 )
 
+type Size struct {
+	x, y int
+}
+
 type sprite struct {
-	name   string
-	img    image.Image
-	bounds image.Rectangle
-	area   int
+	name string
+	img  image.Image
+	rect image.Rectangle
+	area int
+	size Size
 }
 
 type ByArea []sprite
@@ -36,16 +42,31 @@ func main() {
 	files, _ := ioutil.ReadDir(inputDir)
 	sprites := make([]sprite, len(files))
 
+	totalX := 0
+	totalY := 0
+
 	for i := range sprites {
 		s := readSprite(inputDir, files[i].Name())
 		sprites[i] = s
+		totalX += s.size.x
+		totalY += s.size.y
 	}
 
 	// we want to place the largest sprite first
 	sort.Sort(sort.Reverse(ByArea(sprites)))
 
-	for _, s := range sprites {
-		fmt.Println(s)
+	// the final image
+	dst := image.NewRGBA(image.Rect(0, 0, 1024, 1024))
+
+	for i := range sprites {
+		s := &sprites[i]
+		draw.Draw(dst, s.rect, s.img, image.ZP, draw.Src)
+	}
+
+	writer, err := os.Create("test.png")
+	err = png.Encode(writer, dst)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -57,13 +78,15 @@ func readSprite(dir, name string) (s sprite) {
 	}
 	defer reader.Close()
 
-	img, _, err := image.Decode(reader)
+	img, err := png.Decode(reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	s.name = name
 	s.img = img
-	s.bounds = img.Bounds()
-	s.area = s.bounds.Dx() * s.bounds.Dy()
+	s.rect = img.Bounds()
+	s.size = Size{s.rect.Dx(), s.rect.Dy()}
+	s.area = s.size.x * s.size.y
 	return
 }
