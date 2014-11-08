fe704f2da10031d1198cb94e9fe6dffab8981e1e
Pieter Slabbert <zan.xhipe@gmail.com>
2014-11-08 20:00:03 +0200

    Add basic doc.go for godoc

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+atlas-packer [options] spriteDir output
+
+	--dimensions=1024x1024: Size of final atlas
+	--space=1: Space inserted between sprites, in pixels
+
+outputs two files: <output>.png, and <output>.json
+
+spriteDir can only contain sprites, anything else will cause an error.
+*/
+package main
